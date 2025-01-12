89c9fc7a17a5623e2cf129bcee8db5546ecb3b63
Ilham <ilham.yaqubzada.std@bhos.edu.az>
2025-01-12 15:05:32 +0400

    Add files via upload

---
+++ routers/brands.py
@@ -0,0 +1,93 @@
+from typing import List, Annotated
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import text
+from database import sessionLocal
+from models import Product, Category, Brand, User
+from schemas import BrandResponse, BrandBase, BrandCreate
+import logging
+from datetime import datetime
+import pytz
+
+
+TIMEZONE = pytz.timezone("Asia/Baku")
+
+router = APIRouter(
+    prefix="/brands",
+    tags=["brands"]
+)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+logger = logging.getLogger("uvicorn.error")
+
+
+@router.get("", response_model=List[BrandResponse], status_code=status.HTTP_200_OK)
+async def get_all_brands(db: db_dependency):
+    brands = db.query(Brand).order_by(text("date_created DESC")).all()
+    return brands
+
+@router.get("/{brand_id}", response_model=BrandResponse, status_code=status.HTTP_200_OK)
+async def get_brand(brand_id: int, db: db_dependency):
+    brand = db.query(Brand).filter(Brand.id == brand_id).first()
+    if not brand:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Brand not found")
+    return brand
+
+@router.put("/{brand_id}", response_model=BrandResponse, status_code=status.HTTP_200_OK)
+async def update_brand(brand_id: int, brand_data: BrandResponse, db: db_dependency):
+
+    brand = db.query(Brand).filter(Brand.id == brand_id).first()
+    if not brand:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Brand not found")
+ 
+    # Checking brand_name
+    brand_name = db.query(Brand).filter(Brand.name == brand_data.name).first()
+    if brand_name:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Category with name {brand_data.name} exists."
+        )
+    
+    for key, value in brand_data.dict().items():
+        setattr(brand, key, value)
+
+    brand.updated_at = datetime.now(TIMEZONE)
+    db.commit()
+    db.refresh(brand)
+    return brand
+
+
+@router.post("/add", response_model=BrandResponse, status_code=status.HTTP_201_CREATED)
+async def create_brand(brand_data: BrandResponse, db: db_dependency):
+    
+    # Checking ID of Brand
+    brend = db.query(Brand).filter(Brand.id == brand_data.id).first()               
+    if brend:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Brand with id {brand_data.id} exists. Existed brand name -> xxx"
+        )
+    
+    new_brand = Brand(**brand_data.dict())
+    db.add(new_brand)
+    db.commit()
+    db.refresh(new_brand)
+    return new_brand
+
+@router.delete("/{brand_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_brand(brand_id: int, db: db_dependency):
+
+    brand = db.query(Brand).filter(Brand.id == brand_id).first()
+    if not brand:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Brand not found")
+    
+    db.delete(brand)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- routers/products.py
+++ routers/products.py
@@ -3,9 +3,12 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import text
 from database import sessionLocal
-from models import Product, Category, Brand
+from models import Product, Category, Brand, User
 from schemas import ProductBase, ProductCreate, ProductResponse
 import logging
+import pytz
+from datetime import datetime
+
 
 router = APIRouter(
     prefix="/products",
@@ -21,6 +24,7 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 logger = logging.getLogger("uvicorn.error")
+TIMEZONE = pytz.timezone("Asia/Baku")
 
 
 @router.post("/add", response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
@@ -28,7 +32,6 @@ async def create_product(product_data: ProductCreate, db: db_dependency):
     new_product = Product(**product_data.dict())
     
     # Checking category_id 
-    category_id = product_data.category_id
     category = db.query(Category).filter(Category.id == product_data.category_id).first()
     if not category:
         raise HTTPException(
@@ -37,14 +40,22 @@ async def create_product(product_data: ProductCreate, db: db_dependency):
         )
     
     # Checking brend_id
-    brend = db.query(Brend).filter(Brend.id == product_data.brend_id).first()
+    brend = db.query(Brand).filter(Brand.id == product_data.brend_id).first()
     if not brend:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Brend with id {product_data.brend_id} does not exist."
         )
 
-
+    # Checking author_id
+    # author_id = db.query(User).filter(User.id == product_data.author_id).first()
+    # if not author_id:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail=f"Author id : {product_data.brend_id} does not exist."
+    #     )
+    # ????
+    
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
@@ -62,15 +73,34 @@ async def get_product(product_id: int, db: db_dependency):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     return product
 
-
 @router.put("/{product_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
 async def update_product(product_id: int, product_data: ProductCreate, db: db_dependency):
+
     product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     
+     # Checking category_id 
+    category = db.query(Category).filter(Category.id == product_data.category_id).first()
+    if not category:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Category with id {product_data.category_id} does not exist."
+        )
+    
+    # Checking brend_id
+    brend = db.query(Brand).filter(Brand.id == product_data.brend_id).first()
+    if not brend:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Brend with id {product_data.brend_id} does not exist."
+        )
+    
     for key, value in product_data.dict().items():
         setattr(product, key, value)
+
+    product.updated_at = datetime.now(TIMEZONE)
+
 
     db.commit()
     db.refresh(product)
@@ -78,6 +108,7 @@ async def update_product(product_id: int, product_data: ProductCreate, db: db_de
 
 @router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(product_id: int, db: db_dependency):
+
     product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
