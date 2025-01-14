7e1f68d89066c9b133d9d148d34044be112357cc
Ilham <ilham.yaqubzada.std@bhos.edu.az>
2025-01-14 23:08:03 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 import os
-from routers import products, brands
-
+from routers import products, brands, category, p_specifications
 
 app = FastAPI()
 app.add_middleware(
@@ -17,3 +16,5 @@ app.add_middleware(
 
 app.include_router(products.router)
 app.include_router(brands.router)
+app.include_router(category.router)
+app.include_router(p_specifications.router)
