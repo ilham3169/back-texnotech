be0e4c3304f74f1938d8bff6a4e36a9a4e5bdb3f
ilham3169 <ilham.yaqubzada.std@bhos.edu.az>
2025-03-22 17:47:41 +0400

    test3

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 apt-get update && apt-get install -y \
     gcc \
     python3-dev \
     default-libmysqlclient-dev \
-    build-essential
+    build-essential \
+    libmysqlclient-dev
