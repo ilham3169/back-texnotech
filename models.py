0c52ff8b7e0969582a6ca87696167a4cf915e002
ilham3169 <ilham.yaqubzada.std@bhos.edu.az>
2025-05-08 16:26:27 +0400

    coomit - back

--- models.py
+++ models.py
@@ -134,7 +134,7 @@ class Order(Base):
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
     updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
     delivery_method = Column(String(63), nullable=False)
-    month = Column(Integer, nullable=False)
+    # month = Column(Integer, nullable=False)
 
 
     # Relationship with OrderItems
